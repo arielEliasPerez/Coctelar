c8710a2ad6b436db643ba777513ce8f4a04f6911
Ariel <ariel65_ep@hotmail.com>
2024-01-23 17:19:44 -0300

    Agregado de mas cocteles clasicos sin imagenes

--- src/script.js
+++ src/script.js
@@ -1,14 +1,9 @@
-//const coctelesContainer = document.getElementById("cocteles-container");
-//const autorContainer = document.getElementById("autor-container");
 const modal = document.getElementById("receta-modal");
 const coctelTitulo = document.getElementById("coctel-titulo");
 const coctelImagen = document.getElementById("coctel-imagen");
 const coctelIngredientes = document.getElementById("coctel-ingredientes");
 const coctelReceta = document.getElementById("coctel-receta");
 const buscarInput = document.getElementById("buscar-coctel");
-//const verMasClasicos = document.getElementById("ver-mas-clasicos");
-//const verMasAutor = document.getElementById("ver-mas-autor");
-
 
 const categorias = {
    clasicos: {
@@ -28,26 +23,49 @@ const coctelesIniciales = 8;
 // Datos de ejemplo (puedes reemplazarlos con tus propias recetas)
 const coctelesClasicos = [
     { nombre: "Alexander", imagen: "../images/alexander.jpg", ingredientes: ["1 oz Cogñac", "1 oz Licor de Cacao", "1 oz Half and Half"], receta: "En una coctelera con hielo, agrega el coñac, el licor de cacao y el Half and Half.\n Agita vigorosamente durante 15-20 segundos para enfriar y mezclar bien los ingredientes.\n Cuela la mezcla en una Copa Martini o Copa Coupé, previamente enfriada.\n Decorar con una ramita de canela y espolvorear un poco de cacao en polvo por encima.\n ¡Disfruta!" },
+    { nombre: "Americano", imagen: "../images/americano.jpg", ingredientes: ["30 ml de Campari", "30 ml de Vermouth Rosso", "Chorrito de Soda"], receta: "Ingredientes y pasos para la Margarita..." },
+    { nombre: "Barracuda", imagen: "../images/barracuda.jpg", ingredientes: ["45 ml de Ron Blanco", "15 ml de Licor de Banana", "30 ml de Zumo de Ananá", "60 ml de Champagne"], receta: "Ingredientes y pasos para la Margarita..." },
+    { nombre: "Bee's Knees", imagen: "../images/beesknees.jpg", ingredientes: ["50 ml de Gin", "25 ml de Zumo de Limón", "25 ml de Jugo de Naranja", "2 Cucharaditas de Miel"], receta: "Ingredientes y pasos para la Margarita..." },
+    { nombre: "Between the Sheets", imagen: "../images/betweenthesheets.jpg", ingredientes: ["30 ml de Ron", "30 ml de Cognac", "30 ml de Triple Sec", "30 ml de Zumo de Limón"], receta: "Ingredientes y pasos para la Margarita..." },
     { nombre: "Black Russian", imagen: "../images/blackrussian.jpg", ingredientes: ["45 ml de Vodka", "45 ml de Licor de Café", "30 ml de Café (opcional)"], receta: "Ingredientes y pasos para la Margarita..." },
-    { nombre: "Blue Hawaiian", imagen: "../images/bluehawaiian.jpg", ingredientes: ["30 ml de Ron", "15 ml de Blue Curacao", "60 ml de Pulpa de Ananá", "45 ml de Sour Mix"], receta: "Ingredientes y pasos para la Margarita..." },
+    { nombre: "Bloody Mary", imagen: "../images/bloodymary.jpg", ingredientes: ["45 ml de Vodka", "90 ml de Jugo de Tomate", "15 ml de Zumo Limón", "1 Pizca de Sal Fina", "1 Pizca de Pimienta Negra", "1-3 Gotas de Salsa Picante (como Tabasco)"], receta: "Ingredientes y pasos para la Margarita..." },
+    //{ nombre: "Blue Hawaiian", imagen: "../images/bluehawaiian.jpg", ingredientes: ["30 ml de Ron", "15 ml de Blue Curacao", "60 ml de Pulpa de Ananá", "45 ml de Sour Mix"], receta: "Ingredientes y pasos para la Margarita..." },
+    { nombre: "Boulevardier", imagen: "../images/boulevardier.jpg", ingredientes: ["45 ml de Whisky", "30 ml de Campari", "30 ml de Vermouth rosso"], receta: "Ingredientes y pasos para la Margarita..." },
     { nombre: "Caipirinha", imagen: "../images/caipirinha.jpg", ingredientes: ["75 ml de Cachaca", "30 ml de Almibar", "Lima"], receta: "Ingredientes y pasos para la Margarita..." },
     { nombre: "Cosmopolitan", imagen: "../images/cosmopolitan.jpg", ingredientes: ["45 ml de Vodka", "15 ml de Triple Sec", "30 ml de Jugo de Arándano rojo", "15 ml Zumo de Limón"], receta: "Ingredientes y pasos para la Margarita..." },
+    { nombre: "Cuba Libre", imagen: "../images/cubalibre.jpg", ingredientes: ["45 ml de Ron Blanco", "120 ml de Coca-Cola", "10 ml de Zumo de Limón"], receta: "Ingredientes y pasos para la Margarita..." },
+    { nombre: "Daiquiri Clásico", imagen: "../images/daiquiriclasico.jpg", ingredientes: ["60 ml de Ron Blanco", "30 ml de Zumo de Limón", "20 ml de Almíbar"], receta: "Ingredientes y pasos para la Margarita..." },
+    { nombre: "Dry Martini", imagen: "../images/drymartini.jpg", ingredientes:["75 ml de Gin", "15 ml de Vermouth Dry", "Dos o tres Aceitunas"], receta: "Ingredientes y pasos para el Martini..." },
+    { nombre: "Espresso Martini", imagen: "../images/espressomartini.jpg", ingredientes:["45 ml de Vodka", "30 ml de Licor de Café", "30 ml de Café Espresso", "15 ml de Almíbar"], receta: "Ingredientes y pasos para el Martini..." },
     { nombre: "Gancia Batido", imagen: "../images/ganciabatido.jpg", ingredientes: ["75 ml de Gancia", "30 ml de Almibar", "Limón"], receta: "Ingredientes y pasos para la Margarita..." },
+    { nombre: "Hanky Panky", imagen: "../images/hankypanky.jpg", ingredientes: ["45 ml de London Dry Gin", "45 ml de Vermouth Rosso", "10 ml Fernet Branca"], receta: "Ingredientes y pasos para la Margarita..." },
     { nombre: "Long Island Iced Tea", imagen: "../images/longislandicedtea.jpg", ingredientes: ["15 ml de Vodka", "15 ml de Ron", "15 ml de Gin", "15 ml de Triple Sec, 30 ml de Sweet & Sour", "Coca-Cola"], receta: "Ingredientes y pasos para la Margarita..." },
-    { nombre: "Maddy", imagen: "../images/maddy.jpg", ingredientes: ["45 ml de Ron", "15 ml de Licor de Cassis", "30 ml de Jugo de Manzana", "45 ml de Pulpa de Ananá"], receta: "Ingredientes y pasos para la Margarita..." },
-    { nombre: "Mai-Tai", imagen: "../images/maitai.jpg", ingredientes:[], receta: "Ingredientes y pasos para el Martini..." },
+    //{ nombre: "Maddy", imagen: "../images/maddy.jpg", ingredientes: ["45 ml de Ron", "15 ml de Licor de Cassis", "30 ml de Jugo de Manzana", "45 ml de Pulpa de Ananá"], receta: "Ingredientes y pasos para la Margarita..." },
+    { nombre: "Mai-Tai Clásico", imagen: "../images/maitaiclasico.jpg", ingredientes:["45 ml de Ron Blanco", "15 ml de RonOscuro", "15 ml de Amaretto", "15 ml de Triple Sec", "10 ml de Almíbar", "30 ml de Zumo de Limón"], receta: "Ingredientes y pasos para el Martini..." },
     { nombre: "Manhattan", imagen: "../images/manhattan.jpg", ingredientes:["60 ml de Whisky canadian", "30 ml de Vermouth Rosso", "Gotas de Bitter Angostura"], receta: "Ingredientes y pasos para el Martini..." },
     { nombre: "Margarita", imagen: "../images/margarita.jpg", ingredientes:["60 ml de Tequila", "45 ml de Triple Sec", "45 ml de Sour Mix"], receta: "Ingredientes y pasos para la Margarita..." },
-    { nombre: "Martini", imagen: "../images/martini.jpg", ingredientes:["75 ml de Gin", "15 ml de Vermouth Dry", "Dos o tres Aceitunas"], receta: "Ingredientes y pasos para el Martini..." },
+    { nombre: "Martinez", imagen: "../images/martinez.jpg", ingredientes: ["45 ml de London Dry Gin", "45 ml de Vermouth Rosso", "5 ml de Marrasquino", "3 gotas de Bitter Angostura"], receta: "Ingredientes y pasos para la Margarita..." },
+    { nombre: "Mary Pickford", imagen: "../images/marypickford.jpg", ingredientes: ["45 ml de Ron Blanco", "45 ml de Zumo de Piña", "10 ml de Marrasquino", "5 ml de Granadina"], receta: "Ingredientes y pasos para la Margarita..." },
+    { nombre: "Mimosa Clásico", imagen: "../images/mimosaclasico.jpg", ingredientes: ["75 ml de Champagne", "75 ml de Zumo de Naranja"], receta: "Ingredientes y pasos para la Margarita..." },
+    { nombre: "Mint Julep", imagen: "../images/mintjulep.jpg", ingredientes: ["60 ml de Bourbon", "8-10 Hojas de Menta frezca", "10 ml de Almíbar"], receta: "Ingredientes y pasos para la Margarita..." },
+    { nombre: "Moscow Mule", imagen: "../images/moscowmule.jpg", ingredientes: ["60 ml de Vodka", "120 ml de Cerveza de Jengibre", "15 ml de Zumo de Lima"], receta: "Ingredientes y pasos para la Margarita..." },
     { nombre: "Mojito", imagen: "../images/mojito.jpg", ingredientes:["60 ml de Ron", "30 ml de Sour Mix", "6-8 hojas de Menta", "Soda"], receta: "Ingredientes y pasos para el Martini..." },
     { nombre: "Negroni", imagen: "../images/negroni.jpg", ingredientes:["30 ml de Gin", "30 ml de Vermouth Rosso", "30 ml de Campari"], receta: "Ingredientes y pasos para el Martini..." },
+    { nombre: "New York Sour", imagen: "../images/newyorksour.jpg", ingredientes: ["60 ml de Bourbon o Whisky", "30 ml de Zumo de Limón", "20 ml de Almibar", "Clara de Huevo", "15 ml de Vino Tinto (como Merlot o Cabernet Sauvignon)"], receta: "Ingredientes y pasos para la Margarita..." },
+    { nombre: "Old Cuban", imagen: "../images/oldcuban.jpg", ingredientes: ["45 ml de Ron Añejo", "6-8 Hojas de Menta", "30 ml de Zumo de Limón", "20 ml de Almibar", "2 Gotas de Bitter Angostura", "60 ml de Champagne"], receta: "Ingredientes y pasos para la Margarita..." },
     { nombre: "Old Fashioned", imagen: "../images/oldfashioned.jpg", ingredientes:["105 ml de Bourbon", "Una cucharada de Azúcar", "Gotas de Bitter Angostura"], receta: "Ingredientes y pasos para el Martini..." },
     //{ nombre: "Orgasmo", imagen: "../images/orgasmo.jpg", ingredientes: ["45 ml de Vodka", "30 ml de Licor de Durazno", "60 ml de Piña Colada", "15 ml de Granadina"], receta: "Ingredientes y pasos para la Margarita..." },
     { nombre: "Pearl Harbor", imagen: "../images/pearlharbor.jpg", ingredientes: ["30 ml de Vodka", "30 ml de Licor de Melón", "90 ml de Pulpa de Ananá"], receta: "Ingredientes y pasos para la Margarita..." },
+    { nombre: "Penicillin", imagen: "../images/penicillin.jpg", ingredientes: ["60 ml de Whisky scoth", "20 ml de Syrup de Gengibre", "20 ml de Syrup de Miel", "30 ml de Zumo de limón"], receta: "Ingredientes y pasos para la Margarita..." },
     //{ nombre: "Primavera", imagen: "../images/primavera.jpg", ingredientes: ["45 ml de Vodka", "30 ml de Jugo de Naranja", "30 ml de Jugo de Pomelo", "30 ml de Pulpa de Ananá", "15 ml de Pulpa de Frutilla"], receta: "Ingredientes y pasos para la Margarita..." },
     //{ nombre: "Samantha", imagen: "../images/.samanthajpg", ingredientes: ["45 ml de Vodka", "30 ml de Licor de Banana", "60 ml de Piña Colada", "15 ml de Granadina"], receta: "Ingredientes y pasos para la Margarita..." },
+    { nombre: "Sex on the Beach", imagen: "../images/sexonthebeach.jpg", ingredientes: ["45 ml de Vodka", "30 ml de Licor de Durazno", "75 ml de Jugo de Naranja", "10 ml de Granadina"], receta: "Ingredientes y pasos para la Margarita..." },
     { nombre: "Screaming Orgasm", imagen: "../images/screamingorgasm.jpg", ingredientes: ["30 ml de Ron", "30 ml de Bailey's", "30 ml de Amaretto", "30 ml de Licor de Café", "30 ml de Half and Half", "Garnish de Crema"], receta: "Ingredientes y pasos para la Margarita..." },
+    { nombre: "Tequila Sunrise", imagen: "../images/tequilasunrise.jpg", ingredientes: ["45 ml de Tequila", "90 ml de Jugo de Naranja", "10 ml de Granadina"], receta: "Ingredientes y pasos para la Margarita..." },
+    { nombre: "Whiskey Sour", imagen: "../images/whiskeysour.jpg", ingredientes: ["45 ml de Whisky", "30 ml de Zumo de Lima", "20 ml de Almibar Simple", "Clara de Huevo"], receta: "Ingredientes y pasos para la Margarita..." },
+    { nombre: "White Lady", imagen: "../images/whitelady.jpg", ingredientes: ["40 ml de Gin", "30 ml de Triple Sec", "20 ml de Zumo de Limón"], receta: "Ingredientes y pasos para la Margarita..." },
     { nombre: "White Russian", imagen: "../images/whiterussian.jpg", ingredientes: ["30 ml de Vodka", "45 ml de Licor de Café", "30 ml de Half and Half", "30 ml de Café (opcional)"], receta: "Ingredientes y pasos para la Margarita..." },
+    { nombre: "Zombie Clásico", imagen: "../images/zombieclasico.jpg", ingredientes: ["45 ml de Ron Oscuro", "45 ml de Ron Dorado", "20 ml de Zumo de Limón", "15 ml de Falernum", "10 ml de Granadina", "2 gotas de Bitter Angostura"], receta: "Ingredientes y pasos para la Margarita..." },
     // { nombre: "", imagen: "../images/.jpg", ingredientes: [], receta: "Ingredientes y pasos para la Margarita..." },
 ];
 
@@ -76,15 +94,6 @@ function mostrarCocteles(coctelesFiltrados, categoria, inicioCocteles, cantCocte
         const infoCartegoria = categorias[categoria];
 
         infoCartegoria.container.appendChild(coctelCard);
-/*
-        switch(categoria){
-            case Categoria.CLASICOS:
-                coctelesContainer.appendChild(coctelCard);
-                break;
-            case Categoria.DE_AUTOR:
-                autorContainer.appendChild(coctelCard);
-                break;
-        }*/
     });
 }
 
@@ -130,14 +139,6 @@ function filtrarCocteles(cocteles, categoria) {
     infoCartegoria.enlaceVerMas.style.display = "none"; // Ocultar el enlace "Ver más"
     infoCartegoria.container.innerHTML = "";
 
-    /*switch(categoria){
-        case Categoria.CLASICOS:
-            coctelesContainer.innerHTML = "";
-            break;
-        case Categoria.DE_AUTOR:
-            autorContainer.innerHTML = "";
-            break;
-    }*/
     if(buscarInput.value === ""){
         infoCartegoria.mostrarTodos = false;
         infoCartegoria.enlaceVerMas.style.display = "block";
