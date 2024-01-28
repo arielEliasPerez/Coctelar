fa19e06376c1079e8ccb5119a70bbc9a8f8ef6d4
Ariel <ariel65_ep@hotmail.com>
2024-01-28 13:01:30 -0300

    agragado de secciones, correcion bug Ver Mas

--- src/script.js
+++ src/script.js
@@ -11,6 +11,41 @@ const categorias = {
     enlaceVerMas: document.getElementById("ver-mas-clasicos"),
     mostrarTodos: false
    },
+   batidos: {
+    container: document.getElementById("batidos-container"),
+    enlaceVerMas: document.getElementById("ver-mas-batidos"),
+    mostrarTodos: false
+   },
+   refrescados: {
+    container: document.getElementById("refrescados-container"),
+    enlaceVerMas: document.getElementById("ver-mas-refrescados"),
+    mostrarTodos: false
+   },
+   frozzen: {
+    container: document.getElementById("frozzen-container"),
+    enlaceVerMas: document.getElementById("ver-mas-frozzen"),
+    mostrarTodos: false
+   },
+   tikis: {
+    container: document.getElementById("tikis-container"),
+    enlaceVerMas: document.getElementById("ver-mas-tikis"),
+    mostrarTodos: false
+   },
+   cafeteria: {
+    container: document.getElementById("cafeteria-container"),
+    enlaceVerMas: document.getElementById("ver-mas-cafeteria"),
+    mostrarTodos: false
+   },
+   champagne: {
+    container: document.getElementById("champagne-container"),
+    enlaceVerMas: document.getElementById("ver-mas-champagne"),
+    mostrarTodos: false
+   },
+   shooters: {
+    container: document.getElementById("shooters-container"),
+    enlaceVerMas: document.getElementById("ver-mas-shooters"),
+    mostrarTodos: false
+   },
    autor: {
     container: document.getElementById("autor-container"),
     enlaceVerMas: document.getElementById("ver-mas-autor"),
@@ -29,7 +64,6 @@ const coctelesClasicos = [
     { nombre: "Between the Sheets", imagen: "../images/betweenthesheets.jpg", ingredientes: ["30 ml de Ron", "30 ml de Cognac", "30 ml de Triple Sec", "30 ml de Zumo de Limón"], receta: "Ingredientes y pasos para la Margarita..." },
     { nombre: "Black Russian", imagen: "../images/blackrussian.jpg", ingredientes: ["45 ml de Vodka", "45 ml de Licor de Café", "30 ml de Café (opcional)"], receta: "Ingredientes y pasos para la Margarita..." },
     { nombre: "Bloody Mary", imagen: "../images/bloodymary.jpg", ingredientes: ["45 ml de Vodka", "90 ml de Jugo de Tomate", "15 ml de Zumo Limón", "1 Pizca de Sal Fina", "1 Pizca de Pimienta Negra", "1-3 Gotas de Salsa Picante (como Tabasco)"], receta: "Ingredientes y pasos para la Margarita..." },
-    //{ nombre: "Blue Hawaiian", imagen: "../images/bluehawaiian.jpg", ingredientes: ["30 ml de Ron", "15 ml de Blue Curacao", "60 ml de Pulpa de Ananá", "45 ml de Sour Mix"], receta: "Ingredientes y pasos para la Margarita..." },
     { nombre: "Boulevardier", imagen: "../images/boulevardier.jpg", ingredientes: ["45 ml de Whisky", "30 ml de Campari", "30 ml de Vermouth rosso"], receta: "Ingredientes y pasos para la Margarita..." },
     { nombre: "Caipirinha", imagen: "../images/caipirinha.jpg", ingredientes: ["75 ml de Cachaca", "30 ml de Almibar", "Lima"], receta: "Ingredientes y pasos para la Margarita..." },
     { nombre: "Cosmopolitan", imagen: "../images/cosmopolitan.jpg", ingredientes: ["45 ml de Vodka", "15 ml de Triple Sec", "30 ml de Jugo de Arándano rojo", "15 ml Zumo de Limón"], receta: "Ingredientes y pasos para la Margarita..." },
@@ -38,9 +72,9 @@ const coctelesClasicos = [
     { nombre: "Dry Martini", imagen: "../images/drymartini.jpg", ingredientes:["75 ml de Gin", "15 ml de Vermouth Dry", "Dos o tres Aceitunas"], receta: "Ingredientes y pasos para el Martini..." },
     { nombre: "Espresso Martini", imagen: "../images/espressomartini.jpg", ingredientes:["45 ml de Vodka", "30 ml de Licor de Café", "30 ml de Café Espresso", "15 ml de Almíbar"], receta: "Ingredientes y pasos para el Martini..." },
     { nombre: "Gancia Batido", imagen: "../images/ganciabatido.jpg", ingredientes: ["75 ml de Gancia", "30 ml de Almibar", "Limón"], receta: "Ingredientes y pasos para la Margarita..." },
+    { nombre: "Grasshopper", imagen: "../images/grasshopper.jpg", ingredientes: ["30 ml de Vodka", "30 ml de Licor de Chocolate Blanco", "15 ml de Licor de Menta", "15 ml de Half and Half"], receta: "Ingredientes y pasos para la Margarita..." },
     { nombre: "Hanky Panky", imagen: "../images/hankypanky.jpg", ingredientes: ["45 ml de London Dry Gin", "45 ml de Vermouth Rosso", "10 ml Fernet Branca"], receta: "Ingredientes y pasos para la Margarita..." },
     { nombre: "Long Island Iced Tea", imagen: "../images/longislandicedtea.jpg", ingredientes: ["15 ml de Vodka", "15 ml de Ron", "15 ml de Gin", "15 ml de Triple Sec, 30 ml de Sweet & Sour", "Coca-Cola"], receta: "Ingredientes y pasos para la Margarita..." },
-    //{ nombre: "Maddy", imagen: "../images/maddy.jpg", ingredientes: ["45 ml de Ron", "15 ml de Licor de Cassis", "30 ml de Jugo de Manzana", "45 ml de Pulpa de Ananá"], receta: "Ingredientes y pasos para la Margarita..." },
     { nombre: "Mai-Tai Clásico", imagen: "../images/maitaiclasico.jpg", ingredientes:["45 ml de Ron Blanco", "15 ml de RonOscuro", "15 ml de Amaretto", "15 ml de Triple Sec", "10 ml de Almíbar", "30 ml de Zumo de Limón"], receta: "Ingredientes y pasos para el Martini..." },
     { nombre: "Manhattan", imagen: "../images/manhattan.jpg", ingredientes:["60 ml de Whisky canadian", "30 ml de Vermouth Rosso", "Gotas de Bitter Angostura"], receta: "Ingredientes y pasos para el Martini..." },
     { nombre: "Margarita", imagen: "../images/margarita.jpg", ingredientes:["60 ml de Tequila", "45 ml de Triple Sec", "45 ml de Sour Mix"], receta: "Ingredientes y pasos para la Margarita..." },
@@ -54,19 +88,53 @@ const coctelesClasicos = [
     { nombre: "New York Sour", imagen: "../images/newyorksour.jpg", ingredientes: ["60 ml de Bourbon o Whisky", "30 ml de Zumo de Limón", "20 ml de Almibar", "Clara de Huevo", "15 ml de Vino Tinto (como Merlot o Cabernet Sauvignon)"], receta: "Ingredientes y pasos para la Margarita..." },
     { nombre: "Old Cuban", imagen: "../images/oldcuban.jpg", ingredientes: ["45 ml de Ron Añejo", "6-8 Hojas de Menta", "30 ml de Zumo de Limón", "20 ml de Almibar", "2 Gotas de Bitter Angostura", "60 ml de Champagne"], receta: "Ingredientes y pasos para la Margarita..." },
     { nombre: "Old Fashioned", imagen: "../images/oldfashioned.jpg", ingredientes:["105 ml de Bourbon", "Una cucharada de Azúcar", "Gotas de Bitter Angostura"], receta: "Ingredientes y pasos para el Martini..." },
-    //{ nombre: "Orgasmo", imagen: "../images/orgasmo.jpg", ingredientes: ["45 ml de Vodka", "30 ml de Licor de Durazno", "60 ml de Piña Colada", "15 ml de Granadina"], receta: "Ingredientes y pasos para la Margarita..." },
-    { nombre: "Pearl Harbor", imagen: "../images/pearlharbor.jpg", ingredientes: ["30 ml de Vodka", "30 ml de Licor de Melón", "90 ml de Pulpa de Ananá"], receta: "Ingredientes y pasos para la Margarita..." },
     { nombre: "Penicillin", imagen: "../images/penicillin.jpg", ingredientes: ["60 ml de Whisky scoth", "20 ml de Syrup de Gengibre", "20 ml de Syrup de Miel", "30 ml de Zumo de limón"], receta: "Ingredientes y pasos para la Margarita..." },
-    //{ nombre: "Primavera", imagen: "../images/primavera.jpg", ingredientes: ["45 ml de Vodka", "30 ml de Jugo de Naranja", "30 ml de Jugo de Pomelo", "30 ml de Pulpa de Ananá", "15 ml de Pulpa de Frutilla"], receta: "Ingredientes y pasos para la Margarita..." },
-    //{ nombre: "Samantha", imagen: "../images/.samanthajpg", ingredientes: ["45 ml de Vodka", "30 ml de Licor de Banana", "60 ml de Piña Colada", "15 ml de Granadina"], receta: "Ingredientes y pasos para la Margarita..." },
     { nombre: "Sex on the Beach", imagen: "../images/sexonthebeach.jpg", ingredientes: ["45 ml de Vodka", "30 ml de Licor de Durazno", "75 ml de Jugo de Naranja", "10 ml de Granadina"], receta: "Ingredientes y pasos para la Margarita..." },
-    { nombre: "Screaming Orgasm", imagen: "../images/screamingorgasm.jpg", ingredientes: ["30 ml de Ron", "30 ml de Bailey's", "30 ml de Amaretto", "30 ml de Licor de Café", "30 ml de Half and Half", "Garnish de Crema"], receta: "Ingredientes y pasos para la Margarita..." },
     { nombre: "Tequila Sunrise", imagen: "../images/tequilasunrise.jpg", ingredientes: ["45 ml de Tequila", "90 ml de Jugo de Naranja", "10 ml de Granadina"], receta: "Ingredientes y pasos para la Margarita..." },
     { nombre: "Whiskey Sour", imagen: "../images/whiskeysour.jpg", ingredientes: ["45 ml de Whisky", "30 ml de Zumo de Lima", "20 ml de Almibar Simple", "Clara de Huevo"], receta: "Ingredientes y pasos para la Margarita..." },
     { nombre: "White Lady", imagen: "../images/whitelady.jpg", ingredientes: ["40 ml de Gin", "30 ml de Triple Sec", "20 ml de Zumo de Limón"], receta: "Ingredientes y pasos para la Margarita..." },
     { nombre: "White Russian", imagen: "../images/whiterussian.jpg", ingredientes: ["30 ml de Vodka", "45 ml de Licor de Café", "30 ml de Half and Half", "30 ml de Café (opcional)"], receta: "Ingredientes y pasos para la Margarita..." },
     { nombre: "Zombie Clásico", imagen: "../images/zombieclasico.jpg", ingredientes: ["45 ml de Ron Oscuro", "45 ml de Ron Dorado", "20 ml de Zumo de Limón", "15 ml de Falernum", "10 ml de Granadina", "2 gotas de Bitter Angostura"], receta: "Ingredientes y pasos para la Margarita..." },
     // { nombre: "", imagen: "../images/.jpg", ingredientes: [], receta: "Ingredientes y pasos para la Margarita..." },
+];
+
+const coctelesBatidos = [
+    { nombre: "Blue Hawaiian", imagen: "../images/bluehawaiian.jpg", ingredientes: ["30 ml de Ron", "15 ml de Blue Curacao", "60 ml de Pulpa de Ananá", "45 ml de Sour Mix"], receta: "Ingredientes y pasos para la Margarita..." },
+    { nombre: "Chacho", imagen: "../images/chacho.jpg", ingredientes: ["30 ml de Whisky", "45 ml de Licor de Choclote", "15 ml de Licor de Huevo"], receta: "Ingredientes y pasos para la Margarita..." },
+    { nombre: "Cielito Lindo", imagen: "../images/cielitolindo.jpg", ingredientes: ["45 ml de Vodka", "90 ml de Piña Colada", "15 ml de Blue Curacao"], receta: "Ingredientes y pasos para la Margarita..." },
+    { nombre: "Extasis", imagen: "../images/extasis.jpg", ingredientes: ["20 ml de Licor de Kiwi", "20 ml de Licor Durazno", "20 ml de Licor de Frutilla", "20 ml de Licor de Banana", "30 ml de Piña Colada", "30 ml de Pulpa de Ananá"], receta: "Ingredientes y pasos para la Margarita..." },
+    { nombre: "Kriptonita", imagen: "../images/kriptonita.jpg", ingredientes: ["45 ml de Vodka", "90 ml de Piña Colada", "15 ml de Licor de Menta"], receta: "Ingredientes y pasos para la Margarita..." },
+    { nombre: "Maddy", imagen: "../images/maddy.jpg", ingredientes: ["45 ml de Ron", "15 ml de Licor de Cassis", "30 ml de Jugo de Manzana", "45 ml de Pulpa de Ananá"], receta: "Ingredientes y pasos para la Margarita..." },
+    { nombre: "Orgasmo", imagen: "../images/orgasmo.jpg", ingredientes: ["45 ml de Vodka", "30 ml de Licor de Durazno", "60 ml de Piña Colada", "15 ml de Granadina"], receta: "Ingredientes y pasos para la Margarita..." },
+    { nombre: "Pearl Harbor", imagen: "../images/pearlharbor.jpg", ingredientes: ["30 ml de Vodka, ", "30 ml de Licor de Melón", "90 ml de Pulpa de Ananá"], receta: "Ingredientes y pasos para la Margarita..." },
+    { nombre: "Primavera", imagen: "../images/primavera.jpg", ingredientes: ["45 ml de Vodka", "30 ml de Jugo de Naranja", "30 ml de Jugo de Pomelo", "30 ml de Pulpa de Ananá", "15 ml de Pulpa de Frutilla"], receta: "Ingredientes y pasos para la Margarita..." },
+    { nombre: "Samantha", imagen: "../images/samantha.jpg", ingredientes: ["45 ml de Vodka", "30 ml de Licor de Banana", "60 ml de Piña Colada", "15 ml de Granadina"], receta: "Ingredientes y pasos para la Margarita..." },
+    { nombre: "Screaming Orgasm", imagen: "../images/screamingorgasm.jpg", ingredientes: ["30 ml de Ron", "30 ml de Bailey's", "30 ml de Amaretto", "30 ml de Licor de Café", "30 ml de Half and Half", "Garnish de Crema"], receta: "Ingredientes y pasos para la Margarita..." },
+    //{ nombre: "", imagen: "../images/.jpg", ingredientes: [], receta: "Ingredientes y pasos para la Margarita..." },
+];
+
+const coctelesRefrescados = [
+    //{ nombre: "", imagen: "../images/.jpg", ingredientes: [], receta: "Ingredientes y pasos para la Margarita..." },
+];
+
+const coctelesFrozzen = [
+    //{ nombre: "", imagen: "../images/.jpg", ingredientes: [], receta: "Ingredientes y pasos para la Margarita..." },
+];
+
+const coctelesTikis = [
+    //{ nombre: "", imagen: "../images/.jpg", ingredientes: [], receta: "Ingredientes y pasos para la Margarita..." },
+];
+
+const cafeteria = [
+    //{ nombre: "", imagen: "../images/.jpg", ingredientes: [], receta: "Ingredientes y pasos para la Margarita..." },
+];
+
+const coctelesChampagne = [
+    //{ nombre: "", imagen: "../images/.jpg", ingredientes: [], receta: "Ingredientes y pasos para la Margarita..." },
+];
+
+const shooters = [
+    //{ nombre: "", imagen: "../images/.jpg", ingredientes: [], receta: "Ingredientes y pasos para la Margarita..." },
 ];
 
 const coctelesAutor = [
@@ -140,9 +208,12 @@ function filtrarCocteles(cocteles, categoria) {
 
     if(buscarInput.value === ""){
         infoCartegoria.mostrarTodos = false;
-        infoCartegoria.enlaceVerMas.style.display = "block";
-        infoCartegoria.enlaceVerMas.textContent = "Ver más..."
+        
+        infoCartegoria.enlaceVerMas.style.display = coctelesIniciales < cocteles.length ? "block" : "none";
+        infoCartegoria.enlaceVerMas.innerHTML = `Ver más<br>&#11167;`
         mostrarCocteles(coctelesFiltrados, categoria, 0, coctelesIniciales);
+        
+       //mostrarCategoria(cocteles, categoria);
     }else{
         mostrarCocteles(coctelesFiltrados, categoria, 0, coctelesFiltrados.length);
     }
@@ -151,6 +222,13 @@ function filtrarCocteles(cocteles, categoria) {
 
 //manejar eventos de busquedas
 buscarInput.addEventListener("input", ()=>filtrarCocteles(coctelesClasicos, "clasicos"));
+buscarInput.addEventListener("input", ()=>filtrarCocteles(coctelesBatidos, "batidos"));
+buscarInput.addEventListener("input", ()=>filtrarCocteles(coctelesRefrescados, "refrescados"));
+buscarInput.addEventListener("input", ()=>filtrarCocteles(coctelesFrozzen, "frozzen"));
+buscarInput.addEventListener("input", ()=>filtrarCocteles(coctelesTikis, "tikis"));
+buscarInput.addEventListener("input", ()=>filtrarCocteles(cafeteria, "cafeteria"));
+buscarInput.addEventListener("input", ()=>filtrarCocteles(coctelesChampagne, "champagne"));
+buscarInput.addEventListener("input", ()=>filtrarCocteles(shooters, "shooters"));
 buscarInput.addEventListener("input", ()=>filtrarCocteles(coctelesAutor, "autor"));
 
 function mostrarCategoria(cocteles, categoria){
@@ -158,20 +236,22 @@ function mostrarCategoria(cocteles, categoria){
     buscarInput.value = ""; // Asegurarse de que el campo de búsqueda esté vacío al cargar la página
     
     const infoCartegoria = categorias[categoria];
-
+    
     if (coctelesIniciales < cocteles.length) {
         infoCartegoria.enlaceVerMas.style.display = "block";
         infoCartegoria.enlaceVerMas.addEventListener("click", function (event){
             event.preventDefault(); // Evitar el comportamiento predeterminado del enlace
             toggleTodosLosCocteles(cocteles, categoria);
         });
+    } else{
+        infoCartegoria.enlaceVerMas.style.display = "none";
     }
 }
 
 // Agrega esta función al final de tu script.js
 function agregarManejadorDesplazamientoSuave() {
     const enlacesIndice = document.querySelectorAll('#panel-izquierdo a');
-
+    
     enlacesIndice.forEach(enlace => {
         console.log(enlacesIndice[0].textContent);
         enlace.addEventListener('click', function (e) {
@@ -194,6 +274,13 @@ function agregarManejadorDesplazamientoSuave() {
 function iniciarContenido(){
     document.addEventListener("DOMContentLoaded", () => {
         mostrarCategoria(coctelesClasicos, "clasicos");
+        mostrarCategoria(coctelesBatidos, "batidos");
+        mostrarCategoria(coctelesRefrescados, "refrescados");
+        mostrarCategoria(coctelesFrozzen, "frozzen");
+        mostrarCategoria(coctelesTikis, "tikis");
+        mostrarCategoria(cafeteria, "cafeteria");
+        mostrarCategoria(coctelesChampagne, "champagne");
+        mostrarCategoria(shooters, "shooters");
         mostrarCategoria(coctelesAutor, "autor");
         // Llama a la función para agregar el manejador de desplazamiento suave
         agregarManejadorDesplazamientoSuave();
