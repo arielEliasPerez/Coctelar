215a17edf3a013be5d3cead81ed897d245c880ad
Ariel <ariel65_ep@hotmail.com>
2024-01-28 21:44:51 -0300

    aregado de cocteles con cafe

--- src/script.js
+++ src/script.js
@@ -126,6 +126,14 @@ const coctelesTikis = [
 ];
 
 const cafeteria = [
+    { nombre: "Café de la Montaña", imagen: "../images/cafemontaña.jpg", ingredientes: ["30 ml de Licor de Chocolate", "30 ml de Licor de Chocolate Blanco", "150 ml de Café Espresso", "20% de Crema Batida"], receta: "Ingredientes y pasos para la Margarita..." },
+    { nombre: "Café Belga", imagen: "../images/cafebelga.jpg", ingredientes: ["30 ml de Bailey's", "30 ml de Triple Sec", "150 ml de Café Espresso", "20% de Crema Batida"], receta: "Ingredientes y pasos para la Margarita..." },
+    { nombre: "Café Francés", imagen: "../images/cafefrances.jpg", ingredientes: ["60 ml de Brandy o Cognac", "150 ml de Café Espresso", "20% Crema Batida"], receta: "Ingredientes y pasos para la Margarita..." },
+    { nombre: "Café Irlandés", imagen: "../images/cafeirlandes.jpg", ingredientes: ["60 ml de Whisky", "150 ml de Café Espresso", "20% de Crema Batida"], receta: "Ingredientes y pasos para la Margarita..." },
+    { nombre: "Café Marroc", imagen: "../images/cafemarroc.jpg", ingredientes: ["30 ml de Licor de Dulce de Leche", "60 ml de Leche caliente", "120 ml de Café Espresso", "20% Crema Batida"], receta: "Ingredientes y pasos para la Margarita..." },
+    { nombre: "Coco Mocha", imagen: "../images/cocomocha.jpg", ingredientes: ["30 ml de Licor de Coco", "30 ml de Triple Sec", "30 ml de Café frío"], receta: "Ingredientes y pasos para la Margarita..." },
+    { nombre: "Iced Coffee", imagen: "../images/icedcoffee.jpg", ingredientes: ["30 ml de Ron", "30 ml de Licor de Vainilla", "90 ml de Leche fría", "2 cucharaditas de Café Instantáneo"], receta: "Ingredientes y pasos para la Margarita..." },
+    { nombre: "Margarita Coffee", imagen: "../images/margaritacoffee.jpg", ingredientes: ["30 ml de Tequila", "30 ml de Triple Sec", "45 ml de Café Espresso"], receta: "Ingredientes y pasos para la Margarita..." },
     //{ nombre: "", imagen: "../images/.jpg", ingredientes: [], receta: "Ingredientes y pasos para la Margarita..." },
 ];
 
