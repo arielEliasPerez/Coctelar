fe4eb9b22dff3477141fc1958c797aa87125e432
arielEliasPerez <93725336+arielEliasPerez@users.noreply.github.com>
2024-05-30 00:06:10 -0300

    Update coctelesRefrescados.js

--- src/data/cocteles/coctelesRefrescados.js
+++ src/data/cocteles/coctelesRefrescados.js
@@ -1,63 +1,65 @@
+const rutaRefrescados = "src/data/cocteles/imagesCocktail/refrescados/";
+
 const coctelesRefrescados = [
     {
         nombre: "Apple Martini", 
-        imagen: "../src/data/cocteles/imagesCocktail/refrescados/applemartini.jpg", 
+        imagen: rutaRefrescados + "applemartini.jpg", 
         ingredientes: ["45 ml de Vodka", "30 ml de Jugo Manzana", "15 ml de Sour Mix"], 
         receta: "Antes de comenzar pon a enfriar una copa martini.\nLuego vierte todos los ingredientes en un vaso mezclador con hielo. Revuelve suavemente unos 10-15 segundos.\nColar la mezcla en la copa previamente enfriada.\nDecorar con una rodaja de manzana.\n¡Difruta!",
         referencia: "Receta otorgada por el Centro Internacional de Coctelería"
     },
     {
         nombre: "Banana Bliss", 
-        imagen: "../src/data/cocteles/imagesCocktail/refrescados/bananabliss.jpg", 
+        imagen: rutaRefrescados + "bananabliss.jpg", 
         ingredientes: ["45 ml de Brandy", "15 ml de Licor de Banana", "30 ml de Vermouth Bianco"], 
         receta: "Antes de comenzar pon a enfriar una copa martini.\nLuego vierte todos los ingredientes en un vaso mezclador con hielo. Revuelve suavemente unos 10-15 segundos.\nColar la mezcla en la copa previamente enfriada.\nPuedes decorar con una rodaja de banana.\n¡Disfruta!",
         referencia: "Receta otorgada por el Centro Internacional de Coctelería"
     },
     {
         nombre: "Clarito", 
-        imagen: "../src/data/cocteles/imagesCocktail/refrescados/clarito.jpg", 
+        imagen: rutaRefrescados + "clarito.jpg", 
         ingredientes: ["45 ml de Gin", "15 ml de Vermouth Dry", "Twist de Limón"], 
         receta: "Antes de comenzar pon a enfriar una copa martini.\nLuego vierte el gin y el vermouth en un vaso mezclador con hielo. Revuelve suavemente unos 10-15 segundos.\nColar la mezcla en la copa previamente enfriada.\nExprime un twist de cáscara de limón sobre el cóctel para liberar sus aceites esenciales y agrégalo como decoración.\n¡Disfruta!",
         referencia: "Receta otorgada por el Centro Internacional de Coctelería"
     },
     {
         nombre: "Coconut", 
-        imagen: "../src/data/cocteles/imagesCocktail/refrescados/coconut.jpg", 
+        imagen: rutaRefrescados + "coconut.jpg", 
         ingredientes: ["30 ml de Ron", "30 ml de Licor de Coco", "30 ml de Jugo de Manzana"], 
         receta: "Antes de comenzar pon a enfriar una copa martini.\nLuego vierte todos los ingredientes en un vaso mezclador con hielo. Revuelve suavemente unos 10-15 segundos.\nColar la mezcla en la copa previamente enfriada.\nPrevio a colar la mezcla sobre la copa, puedes decorar ésta con ralladura de coco. Tambien puedes colocar una rodaja de manzana.\n¡Disfruta!",
         referencia: "Receta otorgada por el Centro Internacional de Coctelería"
     },
     {
         nombre: "Dreamland", 
-        imagen: "../src/data/cocteles/imagesCocktail/refrescados/dreamland.jpg", 
+        imagen: rutaRefrescados + "dreamland.jpg", 
         ingredientes: ["30 ml de Tequila", "30 ml de Vermouth Bianco", "15 ml de Licor de Vainilla", "15 ml de Zumo de Limón"], 
         receta: "Vierte todos los ingredientes en un vaso mezclador con hielo. Revuelve suavemente unos 10-15 segundos.\nColar la mezcla en un vaso old fashioned.\n¡Disfruta!",
         referencia: "Receta otorgada por el Centro Internacional de Coctelería"
     },
     {
         nombre: "Gibson", 
-        imagen: "../src/data/cocteles/imagesCocktail/refrescados/gibson.jpg", 
+        imagen: rutaRefrescados + "gibson.jpg", 
         ingredientes: ["75 ml de Gin", "15 ml de Vermouth Dry", "Un Cebollín"], 
         receta: "Antes de comenzar pon a enfriar una copa martini.\nLuego vierte el gin y el vermouth en un vaso mezclador con hielo. Revuelve suavemente unos 10-15 segundos.\nColar la mezcla en la copa previamente enfriada.\nAgregue dos o tres cebollines sujetos a un palillo de cóctel.\n¡Disfruta!",
         referencia: "Receta otorgada por el Centro Internacional de Coctelería"
     },
     {
         nombre: "Perfect Manhattan", 
-        imagen: "../src/data/cocteles/imagesCocktail/refrescados/perfectmanhattan.jpg", 
+        imagen: rutaRefrescados + "perfectmanhattan.jpg", 
         ingredientes: ["60 ml de Whisky canadiense", "15 ml de Vermouth Dry", "15 ml de Vermouth Rosso"], 
         receta: "Antes de comenzar pon a enfriar una copa martini.\nLuego vierte todos los ingredientes en un vaso mezclador con hielo. Revuelve suavemente unos 10-15 segundos.\nColar la mezcla en la copa previamente enfriada.\nPuedes decorar con una cereza.\n¡Disfruta!",
         referencia: "Receta otorgada por el Centro Internacional de Coctelería"
     },
     {
         nombre: "Sweet Martini", 
-        imagen: "../src/data/cocteles/imagesCocktail/refrescados/sweetmartini.jpg", 
+        imagen: rutaRefrescados + "sweetmartini.jpg", 
         ingredientes: ["75 ml de Gin", "15 ml de Vermouth Bianco", "Una cereza"], 
         receta: "Antes de comenzar pon a enfriar una copa martini.\nLuego vierte el gin y el vermouth en un vaso mezclador con hielo. Revuelve suavemente unos 10-15 segundos.\nColar la mezcla en la copa previamente enfriada.\nColoca la cereza sobre el cóctel.\n¡Disfruta!",
         referencia: "Receta otorgada por el Centro Internacional de Coctelería"
     },
     {
         nombre: "Vodkatini", 
-        imagen: "../src/data/cocteles/imagesCocktail/refrescados/vodkatini.jpg", 
+        imagen: rutaRefrescados + "vodkatini.jpg", 
         ingredientes: ["75 ml de Vodka", "15 ml de Vermouth Dry", "Twist de Limón"], 
         receta: "Antes de comenzar pon a enfriar una copa martini.\nLuego vierte el vodka y el vermouth en un vaso mezclador con hielo. Revuelve suavemente unos 10-15 segundos.\nColar la mezcla en la copa previamente enfriada.\nExprime un twist de cáscara de limón sobre el cóctel para liberar sus aceites esenciales y agrégalo como decoración.\n¡Disfruta!",
         referencia: "Receta otorgada por el Centro Internacional de Coctelería"
@@ -65,9 +67,10 @@ const coctelesRefrescados = [
     /*
     {
         nombre: "", 
-        imagen: "../src/data/cocteles/imagesCocktail/refrescados/.jpg", 
+        imagen: rutaRefrescados + ".jpg", 
         ingredientes: [""], 
-        receta: "Ingredientes y pasos para la Margarita..." 
+        receta: "Ingredientes y pasos para la Margarita...",
+        referencia:
     },
      */
 ];
