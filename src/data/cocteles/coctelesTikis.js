803d92ed401c4ad88f97fcd737bc972ce1e43be9
arielEliasPerez <93725336+arielEliasPerez@users.noreply.github.com>
2024-05-30 00:11:49 -0300

    Update coctelesTikis.js

--- src/data/cocteles/coctelesTikis.js
+++ src/data/cocteles/coctelesTikis.js
@@ -1,28 +1,30 @@
+const rutaTikis = "src/data/cocteles/imagesCocktail/tikis/";
+
 const coctelesTikis = [
     {
         nombre: "Bahama Mama", 
-        imagen: "../src/data/cocteles/imagesCocktail/tikis/bahamamama.jpg", 
+        imagen: rutaTikis + "bahamamama.jpg", 
         ingredientes: [ "40 ml de Ron blanco", "30 ml de Malibú", "30 ml de Jugo de Ananá", "30 ml de Jugo de Naranja", "15 ml de Granadina"], 
         receta: "Verter todos los ingredientes en una coctelera con hielo. Agitar durante unos 10-15 segundos.\nColar la mezcla en una copa huracán con hielo.\nDecorar con una rodaja de naranja y de ananá.\n¡Disfruta!",
         referencia: "Receta otorgada por el Bartender Jesus Barrero"
     },
     { 
         nombre: "Blue Hawaiian", 
-        imagen: "../src/data/cocteles/imagesCocktail/tikis/bluehawaiian.jpg", 
+        imagen: rutaTikis + "bluehawaiian.jpg", 
         ingredientes: ["30 ml de Ron", "15 ml de Blue Curacao", "60 ml de Pulpa de Ananá", "45 ml de Sour Mix"], 
         receta: "Verter todos los ingredientes en una coctelera con hielo. Agitar durante unos 10-15 segundos.\nColar la mezcla en una copa huracán con hielo.\nDecorar con una rodaja de ananá y una sombrilla tropical.\n¡Disfruta!",
         referencia: "Receta otorgada por el Centro Internacional de Coctelería"
     },
     {
         nombre: "Jungle Bird", 
-        imagen: "../src/data/cocteles/imagesCocktail/tikis/junglebird.jpg", 
+        imagen: rutaTikis + "junglebird.jpg", 
         ingredientes: ["45 ml de Ron oscuro", "20 ml de Campari", "60 ml de Jugo de Ananá", "15 ml de Jugo de Lima", "15 ml de Almibar"], 
         receta: "Verter todos los ingredientes en una coctelera con hielo. Agitar durante unos 10-15 segundos.\nColar la mezcla en un vaso bajo con hielo.\nDecorar con una rodaja y hojas de ananá.\n¡Disfruta!",
         referencia: "Receta establecida por The Educated Barfly"
     },
     {
         nombre: "Mai-Tai Reverted", 
-        imagen: "../src/data/cocteles/imagesCocktail/tikis/maitai.jpg", 
+        imagen: rutaTikis + "maitai.jpg", 
         ingredientes: ["45 ml de Ron", "30 ml de Amaretto", "45 ml de Jugo de Naranja", "30 ml de Jugo de Ananá", "Dash de Granadina (Opcional)"], 
         receta: "En un vaso alto, preferentemente con temática Tiki, con hielo, verter todos los ingredientes, dejando el chorrito de granadina para lo último.\nDecorar con frutas tropicales\n¡Disfruta!",
         referencia: "Receta otorgada por el Centro Internacional de Coctelería"
@@ -36,21 +38,21 @@ const coctelesTikis = [
     },
     {
         nombre: "Médico Brujo", 
-        imagen: "../src/data/cocteles/imagesCocktail/tikis/medicobrujo.jpg",
+        imagen: rutaTikis + "medicobrujo.jpg",
         ingredientes: ["30 ml de Capitán Morgan Silver", "30 ml de Capitán Morgan Spiced", "22,5 ml de Jugo de Ananá", "22,5 ml de Zumo de Pomelo", "22,5 ml de Syrup de Canela", "7,5 ml de Jugo de Limón", "7,5 ml de Jaggermeister"], 
         receta: "En una coctelera con hielo, verter todos los ingredientes y batir fuerte por 10 segundos. Servir en un vaso mug de calavera.\n¡Disfruta!",
         referencia: "Receta Oficial de BRUKBAR, by Santiago Cánepa"
     },
     {
         nombre: "Navy Grog", 
-        imagen: "../src/data/cocteles/imagesCocktail/tikis/navygrog.jpg", 
+        imagen: rutaTikis + "navygrog.jpg", 
         ingredientes: ["30 ml de Ron oscuro", "30 ml de Ron blanco", "30 ml de Jugo de Limón", "20 ml de Miel", "30 ml de Jugo de Pomelo", "Rellenar con Soda (Opcional)"], 
         receta: "En una coctelera con hielo, verter todos los ingredientes excepto la soda. Agitar durante unos 15-20 segundos.\nColar la mezcla en un vaso alto con hielo. Luego rellenar con la soda.\nDecorar con una rodaja de pomelo o limón y unas hojas de menta.\n¡Disfruta!",
         referencia: "Basada en la receta de Steve Bartender"
     },
     {
         nombre: "Shark", 
-        imagen: "../src/data/cocteles/imagesCocktail/tikis/shark.jpg", 
+        imagen: rutaTikis + "shark.jpg", 
         ingredientes: ["30 ml de Ron Oscuro", "30 ml de Ron Blanco", "30 ml de Vermouth Rosso", "30 ml de Cynar", "30 ml de Jugo de Frambueza", "45 ml de Pulpa de Ananá", "Gotas de Bitter Angostura"], 
         receta: "Verter todos los ingredientes en una coctelera con hielo. Agitar durante unos 10-15 segundos.\nColar la mezcla en un vaso mug de tiburón con hielo.\nPuedes decorar con unas hojas de ananá.\n¡Disfruta!",
         referencia: "Basado en la receta de Oh No! Lulu Tiki Bar"
@@ -58,9 +60,10 @@ const coctelesTikis = [
     /*
     {
         nombre: "", 
-        imagen: "../src/data/cocteles/imagesCocktail/tikis/.jpg", 
+        imagen: rutaTikis + ".jpg", 
         ingredientes: [""], 
-        receta: "Ingredientes y pasos para la Margarita..." 
+        receta: "Ingredientes y pasos para la Margarita...",
+        referencia:
     },
     */
 ];
